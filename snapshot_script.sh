5953cef030d23bf6eeabd5b300599a1f21cd9fc8
jlm9 <42284773+jlm9@users.noreply.github.com>
2020-02-25 01:22:15 -0500

    Update snapshot_script.sh

--- snapshot_script.sh
+++ snapshot_script.sh
@@ -8,7 +8,7 @@ echo "Enter in the last vmid that you want to snapshot"
 
 read endid
 
-echo "Enter in the name of the snapshot, cannot be more than one word"
+echo "Enter in the name of the snapshot, cannot be more than one word, or can be written like: For_Example"
 
 read snapname
 
@@ -20,6 +20,7 @@ while [ $beginid -le $endid ]
 
 do
         echo $beginid
+        # qm snapshot vmid name of snapshot --description of snapshot --vmstate preserve
         qm snapshot $beginid "$snapname" --description "$description" --vmstate true
         beginid=$(($beginid + 1))
 done
