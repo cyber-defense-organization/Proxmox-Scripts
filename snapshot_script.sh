0ad6288b6a16ddfb31e94bfb39345cffd405ab32
jlm9 <42284773+jlm9@users.noreply.github.com>
2020-02-25 01:03:01 -0500

    Update snapshot_script.sh

--- snapshot_script.sh
+++ snapshot_script.sh
@@ -3,21 +3,18 @@
 echo "Enter in the beginning vmid that you want to snapshot"
 
 read beginid
-echo $beginid
 
 echo "Enter in the last vmid that you want to snapshot"
 
 read endid
-echo $endid
 
 echo "Enter in the name of the snapshot, cannot be more than one word"
 
 read snapname
-echo $snapname
+
 echo "enter in a description of the snapshot (mandatory)"
 
 read description
-echo $description
 
 while [ $beginid -le $endid ]
 
